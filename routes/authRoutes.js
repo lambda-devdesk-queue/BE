b9bbd16a87fa28de6d70eaa4aaf5faf5d7c8a6d8
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-04-30 00:18:06 -0500

    Login route returns token

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const jwtKey = process.env.JWT_SECRET || `add a .env file to the root of the project with a JWT_SECRET variable`;
+
 const router = express.Router();
 
 const Tickets = require('../models/ticketsModel');
@@ -31,8 +35,40 @@ router.post('/register', async (req, res) => {
   }
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
+  const userLogin = req.body;
 
+  const user = await Users.login(userLogin.username);
+
+  if(user){
+    if(bcrypt.compareSync(userLogin.password, user.password)){
+      const token = generateToken(user);
+
+      res.status(200).json({
+          message: 'Login successful',
+          token: token,
+          user_id: user.id
+      });
+    } else {
+      res.status(404).json({msg: 'Invalid username or password'});
+    }
+  } else {
+    res.status(500).json({msg: 'Login unsucessful'});
+  }
 });
+
+generateToken = (user) => {
+    const payload = {
+        username: user.username,
+        role: user.role,
+        user_id: user.id,
+    }
+
+    const options = {
+        expiresIn: '3h'
+    }
+
+    return jwt.sign(payload, jwtKey, options)
+}
 
 module.exports = router;
